d17f029ef62e5d0aa0fd0c10ebe3634a553ca872
soumic sarkar <soumic@codillion.com>
2025-03-24 01:30:59 +0530

    Added basic project variables

--- hoh-frontend/src/app/page.tsx
+++ hoh-frontend/src/app/page.tsx
@@ -3,11 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-row items-center justify-center w-full h-screen">
-      <h1 className="text-white underline">
-        HOH- Heros of Humanity
-
-      </h1>
-
+      <h1 className="text-black underline">HOH- Heros of Humanity</h1>
     </div>
   );
 }
