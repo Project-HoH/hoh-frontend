5ab64e0f5fd5fb6a516473ff6df1e38aec98f7b2
soumic sarkar <soumic@codillion.com>
2025-03-24 18:58:44 +0530

    Commit

--- src/app/components/landing-page/FlipWord.tsx
+++ src/app/components/landing-page/FlipWord.tsx
@@ -1,11 +1,11 @@
 import { FlipWords } from "@/components/ui/flip-words";
 
 const FlipWord = () => {
-  const words = ["Best Chance", "Dearest Friend", "Worst Enemy", "Savior"];
+  const words = ["Best Chance_", "Dearest Friend", "Worst Enemy", "Savior"];
   return (
     <div className="flex flex-row items-center justify-center overflow-hidden md:-my-[120px]">
-      <h1 className="font-extrabold text-[60px] font-sans ">
-        You Are Your Own , <FlipWords words={words} />
+      <h1 className="font-extrabold text-[60px] font-sans text-[#881414] ">
+        You Are Your Own , <FlipWords words={words} className="text-[#881414] underline" />
       </h1>
     </div>
   );
